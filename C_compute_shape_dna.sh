9c817eb5a897a22ebafafdcdaba77c09d283803d
francopestilli <frakkopesto@gmail.com>
2017-04-14 12:55:36 -0400

    ENH: started cleaning comments and files

--- C_compute_shape_dna.sh
+++ C_compute_shape_dna.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-export SHAPEDNA_HOME=/Users/lindseykitchell/Documents/Github_repos/shapeDNA-tria
+# This script will run Shape DNA of a series of meshes (.off files) geenrated with A_* and B_*
+#
+# The result is a series of stats saved into a *.ev (Eigen Values) file
+#
+# Lindsey Kitchell, Brain-Life Team, Indiana University, Copyright 2017 
 
+export SHAPEDNA_HOME=/Users/lindseykitchell/Documents/Github_repos/shapeDNA-tria
 for f in *.off;
 do
     $SHAPEDNA_HOME/shapeDNA-tria --mesh $f --num 50 --normfirst
